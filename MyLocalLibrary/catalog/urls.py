9f2ef2b56550f7a28681c0d33a27756f2305e63c
Piyush Ranjan <piyush.rnjn@gmail.com>
2019-07-14 17:57:16 +0530

    First Upload

---
+++ MyLocalLibrary/catalog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from catalog import views
+
+
+urlpatterns = [
+
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('borrowed/', views.AllBorrowed.as_view(), name='all-borrowed'),
+    path('authors/', views.AuthorList.as_view(), name='author-list'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian')
+]
